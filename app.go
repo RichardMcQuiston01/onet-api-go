e4a6ddec87d3c501707464d401d86ce613fd4caf
Richard McQuiston <richard.m@engagelearningsystems.com>
2024-04-17 06:40:00 -0400

    Initial

---
+++ app.go
@@ -0,0 +1,60 @@
+package onet_api_go
+
+import (
+	"encoding/base64"
+	"github.com/joho/godotenv"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func goDotEnvVariable(envFilename string, key string) string {
+
+	// load .env file
+	err := godotenv.Load(envFilename)
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func redirectPolicyFunc(req *http.Request, via []*http.Request) error {
+	var environmentFile = ".env.develop"
+	godotenv.Load(environmentFile)
+
+	onetUsername := goDotEnvVariable(environmentFile, "USERNAME")
+	onetPassword := goDotEnvVariable(environmentFile, "PASSWORD")
+	req.Header.Add("Authorization", "Basic "+basicAuth(onetUsername, onetPassword))
+	return nil
+}
+
+func main() {
+	var environmentFile = ".env.develop"
+	godotenv.Load(environmentFile)
+
+	onetUsername := goDotEnvVariable(environmentFile, "USERNAME")
+	onetPassword := goDotEnvVariable(environmentFile, "PASSWORD")
+	//baseUrl := goDotEnvVariable(environmentFile, "BASEURL")
+
+	client := &http.Client{
+		CheckRedirect: redirectPolicyFunc,
+	}
+	req, err := http.NewRequest("GET", "https://services.onetcenter.org/ws/mnm/interestprofiler/job_zones", nil)
+	req.Header.Add("Authorization", "Basic "+basicAuth(onetUsername, onetPassword))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyText, err := ioutil.ReadAll(resp.Body)
+
+	log.Print(bodyText)
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module onet-api-go
+
+go 1.22.0
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+)
